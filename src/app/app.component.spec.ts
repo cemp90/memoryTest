ffa2c960aafe9957836defcd879306b637fab979
Tu Nombre <you@example.com>
2023-11-01 07:57:11 +0100

    se agrega codigo de otro proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'memoryTest'`, () => {
+  it(`should have as title 'memoria'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('memoryTest');
+    expect(app.title).toEqual('memoria');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('memoryTest app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('memoria app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription, timer } from 'rxjs';
+import { SpeechService } from './speech/services/speech.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'memoryTest';
+  mapPalabras: Map<string, string[]> = new Map<string, string[]>();
+
+  intervaloRecarga: number = 0;
+
+  indiceLista: string = '';
+
+  palabras: string[] | undefined = [];
+
+  palabra: string = '';
+
+
+  timer: any;
+  timerSubscription: any = new Subscription();
+
+  constructor(private speechService: SpeechService) {
+    this.mapPalabras.set("1", ["Ahorcar", "Violencia", "Clave", "Escuela", "Sostener", "Naturaleza", "Chillar", "Galopar", "Grano", "Entumecido"]);
+    this.mapPalabras.set("2", ["Ahorcar", "Violencia", "Clave", "Escuela", "Sostener", "Naturaleza", "Chillar", "Galopar", "Grano", "Entumecido"]);
+    this.mapPalabras.set("3", ["Ahorcar", "Violencia", "Clave", "Escuela", "Sostener", "Naturaleza", "Chillar", "Galopar", "Grano", "Entumecido"]);
+    this.mapPalabras.set("4", ["Ahorcar", "Violencia", "Clave", "Escuela", "Sostener", "Naturaleza", "Chillar", "Galopar", "Grano", "Entumecido"]);
+    this.mapPalabras.set("5", ["Ahorcar", "Violencia", "Clave", "Escuela", "Sostener", "Naturaleza", "Chillar", "Galopar", "Grano", "Entumecido"]);
+    this.mapPalabras.set("6", ["Ahorcar", "Violencia", "Clave", "Escuela", "Sostener", "Naturaleza", "Chillar", "Galopar", "Grano", "Entumecido"]);
+    this.mapPalabras.set("7", ["Ahorcar", "Violencia", "Clave", "Escuela", "Sostener", "Naturaleza", "Chillar", "Galopar", "Grano", "Entumecido"]);
+  }
+
+  recuperarOpcionesListas(): string[] {
+    let claves: string[] = [];
+    for (const [key] of this.mapPalabras) {
+      claves.push(key);
+    }
+    return claves;
+  }
+
+  comenzarTest() {
+
+    this.speechService.setVoices(speechSynthesis.getVoices().filter((voice) => voice.lang.includes('es')));
+
+    this.speechService.updateVoice('Spanish (Latin America)+Jacky');
+
+    this.speechService.updateSpeech({ name: 'rate', value: '1' });
+
+    this.speechService.updateSpeech({ name: 'pitch', value: '1' });
+
+    this.palabras = this.mapPalabras.get(this.indiceLista);
+
+    this.speechService.updateSpeech({ name: 'text', value: this.palabra });
+
+    this.timerSubscription.add((startOver: boolean | undefined) => this.speechService.toggle(startOver));
+
+    this.timer = timer(3000, this.intervaloRecarga);
+    this.timerSubscription = this.timer.subscribe((t: any) => {
+      this.palabra = this.recuperarPalabra(t);
+
+      this.speechService.updateSpeech({ name: 'text', value: this.palabra });
+
+
+    });
+  }
+
+  recuperarPalabra(t: any): string {
+    if (t < 10) {
+      if (this.palabras != undefined) {
+        return this.palabras[t];
+      } else {
+        return '';
+      }
+
+    }
+    this.timerSubscription.unsubscribe();
+    return 'FIN!';
+
+  }
+
 }
